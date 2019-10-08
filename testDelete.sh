16acd35db67b7f147e9101268555b733b9e9024d
Nitieii <54606981+Nitieii@users.noreply.github.com>
2019-10-08 21:55:35 +0900

    Update testDelete.sh

--- testDelete.sh
+++ testDelete.sh
@@ -8,7 +8,7 @@ cd ../
 cd db
 bash deleteCar.sh $1
 
-if grep -q "$carDelete" "maincarfile.txt"; then
+if grep -q "$carDelete" "maincarfile.txt" && -f "/Users/m19-018/Desktop/ComSciIsak/carRentalApp/db/car/$carDelete.sh"; then
   echo "Test failed"
 else
   echo "Test : passed"
